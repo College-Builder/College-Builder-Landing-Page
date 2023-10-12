2ec095aabed0d3da911cbd60a33ee966ab2e5af2
Gabriel Flores <108844543+GabrielFlores8227@users.noreply.github.com>
2023-10-12 19:02:43 -0300

    update

--- views/root/scripts/index.js
+++ views/root/scripts/index.js
@@ -25,3 +25,58 @@
 		state = !state;
 	}
 })();
+
+(() => {
+	const button = window.document.querySelector('button[image-carousel-button]');
+	const images = button.querySelectorAll('img');
+	let imageIndex = 0;
+	let goRight = true;
+	let interval = setInterval(() => handleScroll(), 8000);
+
+	button.addEventListener('click', () => handleScroll());
+
+	window.addEventListener('resize', () => {
+		imageIndex = 0;
+		goRight = true;
+		button.scrollTo({
+			top: 0,
+			left: 0,
+			behavior: 'smooth',
+		});
+	});
+
+	function handleScroll() {
+		clearInterval(interval);
+		interval = setInterval(() => handleScroll(), 8000);
+
+		let left;
+
+		if (goRight) {
+			left =
+				button.scrollLeft +
+				Number(window.getComputedStyle(button).width.replace('px', ''));
+
+			imageIndex++;
+		} else {
+			left =
+				button.scrollLeft -
+				Number(window.getComputedStyle(button).width.replace('px', ''));
+
+			imageIndex--;
+		}
+
+		if (imageIndex === 0) {
+			goRight = true;
+		}
+
+		if (imageIndex === images.length - 1) {
+			goRight = false;
+		}
+
+		button.scrollTo({
+			top: 0,
+			left,
+			behavior: 'smooth',
+		});
+	}
+})();
