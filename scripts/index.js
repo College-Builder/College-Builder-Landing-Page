b34b0f2802b91796151247d68170833f9fcb3b37
GabrielFlores8227 <jgdaflores184186@hotmail.com>
2023-11-30 01:03:28 -0300

    update

--- scripts/index.js
+++ scripts/index.js
@@ -1,261 +1,270 @@
 (() => {
-	const button = window.document.querySelector('button[menu-button]');
-	const buttonOff = window.document.querySelector('button[menu-button-off]');
-	const menu = window.document.querySelector('div[menu-container]');
-	const menuHeight = window.getComputedStyle(menu).height;
+  const button = window.document.querySelector("button[menu-button]");
+  const buttonOff = window.document.querySelector("button[menu-button-off]");
+  const menu = window.document.querySelector("div[menu-container]");
+  const menuHeight = window.getComputedStyle(menu).height;
 
-	menu.style.height = '0px';
+  menu.style.height = "0px";
 
-	let state = false;
+  let state = false;
 
-	button.addEventListener('click', () => handleDrawerAction());
-	buttonOff.addEventListener('click', () => handleDrawerAction());
+  button.addEventListener("click", () => handleDrawerAction());
+  buttonOff.addEventListener("click", () => handleDrawerAction());
 
-	menu.querySelectorAll('a').forEach((a) => {
-		a.addEventListener('click', () => {
-			handleDrawerAction();
-		});
-	});
+  menu.querySelectorAll("a").forEach((a) => {
+    a.addEventListener("click", () => {
+      handleDrawerAction();
+    });
+  });
 
-	function handleDrawerAction() {
-		if (state) {
-			menu.style.height = '0px';
-			buttonOff.classList.remove('--on');
-			button.classList.remove('--on');
-		} else {
-			menu.style.height = menuHeight;
-			buttonOff.classList.add('--on');
-			button.classList.add('--on');
-		}
+  function handleDrawerAction() {
+    if (state) {
+      menu.style.height = "0px";
+      buttonOff.classList.remove("--on");
+      button.classList.remove("--on");
+    } else {
+      menu.style.height = menuHeight;
+      buttonOff.classList.add("--on");
+      button.classList.add("--on");
+    }
 
-		state = !state;
-	}
+    state = !state;
+  }
 })();
 
 (() => {
-	window.document.querySelectorAll('a[content-link]').forEach((a) => {
-		a.addEventListener('click', (e) => {
-			e.preventDefault();
-			handleClickScroll(a.getAttribute('href'));
-		});
-	});
+  var fullURL = window.location.href;
 
-	function handleClickScroll(objectRef) {
-		const object = window.document.querySelector(
-			`[id='${objectRef.replace('#', '')}']`,
-		);
+  if (fullURL.includes("#")) {
+    var fragment = fullURL.split("#")[1];
+    setTimeout(() => {
+      handlePageScroll(`#${fragment}`);
+    }, 300);
+  }
 
-		if (!object) {
-			return;
-		}
+  window.document.querySelectorAll("a[content-link]").forEach((a) => {
+    a.addEventListener("click", (e) => {
+      e.preventDefault();
+      handlePageScroll(a.getAttribute("href"));
+    });
+  });
 
-		const rect = object.getBoundingClientRect();
-		const yCoordinate = rect.top + window.scrollY;
-		const screenHeight = window.innerHeight;
+  function handlePageScroll(objectRef) {
+    const object = window.document.querySelector(
+      `[id='${objectRef.replace("#", "")}']`
+    );
 
-		if (rect.height < screenHeight && window.innerWidth > 1300) {
-			// If object height is less than screen height, scroll to center of the screen
-			const centerYCoordinate = yCoordinate - (screenHeight - rect.height) / 2;
-			window.scrollTo({
-				behavior: 'smooth',
-				top: centerYCoordinate,
-				left: 0,
-			});
-		} else {
-			// If object height is greater than screen height, scroll to the beginning of the object
-			window.scrollTo({
-				behavior: 'smooth',
-				top: yCoordinate - 50,
-				left: 0,
-			});
-		}
-	}
+    if (!object) {
+      return;
+    }
+
+    const rect = object.getBoundingClientRect();
+    const yCoordinate = rect.top + window.scrollY;
+    const screenHeight = window.innerHeight;
+
+    if (rect.height < screenHeight && window.innerWidth > 1300) {
+      // If object height is less than screen height, scroll to center of the screen
+      const centerYCoordinate = yCoordinate - (screenHeight - rect.height) / 2;
+      window.scrollTo({
+        behavior: "smooth",
+        top: centerYCoordinate,
+        left: 0,
+      });
+    } else {
+      // If object height is greater than screen height, scroll to the beginning of the object
+      window.scrollTo({
+        behavior: "smooth",
+        top: yCoordinate - 50,
+        left: 0,
+      });
+    }
+  }
 })();
 
 (() => {
-	const projectsContainer = window.document.querySelector(
-		'div[projects-container]',
-	);
-	const numberOfProjects = projectsContainer.querySelectorAll('a').length;
-	const nextButtons = window.document.querySelectorAll(
-		'button[section-projects__next-button]',
-	);
+  const projectsContainer = window.document.querySelector(
+    "div[projects-container]"
+  );
+  const numberOfProjects = projectsContainer.querySelectorAll("a").length;
+  const nextButtons = window.document.querySelectorAll(
+    "button[section-projects__next-button]"
+  );
 
-	nextButtons[0].classList.add('--off');
+  nextButtons[0].classList.add("--off");
 
-	if (numberOfProjects === 1) {
-		nextButtons[1].classList.add('--off');
-		return;
-	}
+  if (numberOfProjects === 1) {
+    nextButtons[1].classList.add("--off");
+    return;
+  }
 
-	let index = 0;
+  let index = 0;
 
-	let left = false;
-	let time = 6000;
+  let left = false;
+  let time = 6000;
 
-	let run = true;
-	let runSet = false;
-	let runAgain;
+  let run = true;
+  let runSet = false;
+  let runAgain;
 
-	nextButtons.forEach((button, index) => {
-		button.addEventListener('click', () => {
-			run = false;
+  nextButtons.forEach((button, index) => {
+    button.addEventListener("click", () => {
+      run = false;
 
-			if (runAgain) {
-				runSet = false;
-				clearTimeout(runAgain);
-			}
+      if (runAgain) {
+        runSet = false;
+        clearTimeout(runAgain);
+      }
 
-			if (index === 0) {
-				moveLeft();
-			} else {
-				moveRight();
-			}
-		});
-	});
+      if (index === 0) {
+        moveLeft();
+      } else {
+        moveRight();
+      }
+    });
+  });
 
-	setInterval(() => {
-		if (!run) {
-			if (!runSet) {
-				runSet = true;
+  setInterval(() => {
+    if (!run) {
+      if (!runSet) {
+        runSet = true;
 
-				runAgain = setTimeout(() => {
-					run = true;
-					runSet = false;
-				}, time * 5);
-			}
+        runAgain = setTimeout(() => {
+          run = true;
+          runSet = false;
+        }, time * 5);
+      }
 
-			return;
-		}
+      return;
+    }
 
-		if (left) {
-			moveLeft();
-		} else {
-			moveRight();
-		}
+    if (left) {
+      moveLeft();
+    } else {
+      moveRight();
+    }
 
-		if (index === numberOfProjects - 1) {
-			left = true;
-		}
+    if (index === numberOfProjects - 1) {
+      left = true;
+    }
 
-		if (index === 0) {
-			left = false;
-		}
-	}, time);
+    if (index === 0) {
+      left = false;
+    }
+  }, time);
 
-	window.addEventListener('resize', () => {
-		nextButtons[0].classList.add('--off');
-		nextButtons[1].classList.remove('--off');
+  window.addEventListener("resize", () => {
+    nextButtons[0].classList.add("--off");
+    nextButtons[1].classList.remove("--off");
 
-		projectsContainer.scrollTo({
-			behavior: 'smooth',
-			left: 0,
-		});
-	});
+    projectsContainer.scrollTo({
+      behavior: "smooth",
+      left: 0,
+    });
+  });
 
-	function moveLeft() {
-		const projectsContainerWidth =
-			window.getComputedStyle(projectsContainer).width;
+  function moveLeft() {
+    const projectsContainerWidth =
+      window.getComputedStyle(projectsContainer).width;
 
-		index--;
+    index--;
 
-		if (index === 0) {
-			nextButtons[0].classList.add('--off');
-		}
+    if (index === 0) {
+      nextButtons[0].classList.add("--off");
+    }
 
-		nextButtons[1].classList.remove('--off');
+    nextButtons[1].classList.remove("--off");
 
-		projectsContainer.scrollTo({
-			behavior: 'smooth',
-			left:
-				projectsContainer.scrollLeft -
-				Number(projectsContainerWidth.replace('px', '')),
-		});
-	}
+    projectsContainer.scrollTo({
+      behavior: "smooth",
+      left:
+        projectsContainer.scrollLeft -
+        Number(projectsContainerWidth.replace("px", "")),
+    });
+  }
 
-	function moveRight() {
-		const projectsContainerWidth =
-			window.getComputedStyle(projectsContainer).width;
+  function moveRight() {
+    const projectsContainerWidth =
+      window.getComputedStyle(projectsContainer).width;
 
-		index++;
+    index++;
 
-		if (index === numberOfProjects - 1) {
-			nextButtons[1].classList.add('--off');
-		}
+    if (index === numberOfProjects - 1) {
+      nextButtons[1].classList.add("--off");
+    }
 
-		nextButtons[0].classList.remove('--off');
+    nextButtons[0].classList.remove("--off");
 
-		projectsContainer.scrollTo({
-			behavior: 'smooth',
-			left:
-				projectsContainer.scrollLeft +
-				Number(projectsContainerWidth.replace('px', '')),
-		});
-	}
+    projectsContainer.scrollTo({
+      behavior: "smooth",
+      left:
+        projectsContainer.scrollLeft +
+        Number(projectsContainerWidth.replace("px", "")),
+    });
+  }
 })();
 
 (() => {
-	const button = window.document.querySelector('button[image-carousel-button]');
-	const images = button.querySelectorAll('img');
-	let imageIndex = 0;
-	let goRight = true;
-	let wait = false;
-	let interval = setInterval(() => handleScroll(), 8000);
+  const button = window.document.querySelector("button[image-carousel-button]");
+  const images = button.querySelectorAll("img");
+  let imageIndex = 0;
+  let goRight = true;
+  let wait = false;
+  let interval = setInterval(() => handleScroll(), 8000);
 
-	button.addEventListener('click', () => handleScroll());
+  button.addEventListener("click", () => handleScroll());
 
-	window.addEventListener('resize', () => {
-		imageIndex = 0;
-		goRight = true;
-		button.scrollTo({
-			top: 0,
-			left: 0,
-			behavior: 'smooth',
-		});
-	});
+  window.addEventListener("resize", () => {
+    imageIndex = 0;
+    goRight = true;
+    button.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: "smooth",
+    });
+  });
 
-	function handleScroll() {
-		if (wait) {
-			return;
-		}
+  function handleScroll() {
+    if (wait) {
+      return;
+    }
 
-		wait = true;
+    wait = true;
 
-		setTimeout(() => {
-			wait = false;
-		}, 600);
+    setTimeout(() => {
+      wait = false;
+    }, 600);
 
-		clearInterval(interval);
-		interval = setInterval(() => handleScroll(), 8000);
+    clearInterval(interval);
+    interval = setInterval(() => handleScroll(), 8000);
 
-		let left;
+    let left;
 
-		if (goRight) {
-			left =
-				button.scrollLeft +
-				Number(window.getComputedStyle(button).width.replace('px', ''));
+    if (goRight) {
+      left =
+        button.scrollLeft +
+        Number(window.getComputedStyle(button).width.replace("px", ""));
 
-			imageIndex++;
-		} else {
-			left =
-				button.scrollLeft -
-				Number(window.getComputedStyle(button).width.replace('px', ''));
+      imageIndex++;
+    } else {
+      left =
+        button.scrollLeft -
+        Number(window.getComputedStyle(button).width.replace("px", ""));
 
-			imageIndex--;
-		}
+      imageIndex--;
+    }
 
-		if (imageIndex === 0) {
-			goRight = true;
-		}
+    if (imageIndex === 0) {
+      goRight = true;
+    }
 
-		if (imageIndex === images.length - 1) {
-			goRight = false;
-		}
+    if (imageIndex === images.length - 1) {
+      goRight = false;
+    }
 
-		button.scrollTo({
-			top: 0,
-			left,
-			behavior: 'smooth',
-		});
-	}
+    button.scrollTo({
+      top: 0,
+      left,
+      behavior: "smooth",
+    });
+  }
 })();
